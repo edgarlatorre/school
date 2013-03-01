241641af2c0d7656b890521a73fac437e2a6af41
Edgar <me@edgarlatorre.com.br>
2013-03-01 15:28:35 +0100

    estrutura da aplicacao

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+School::Application.config.secret_token = '7b7b7731b42c78d0e97db3d2a7549eea137c600a004e91f4347821346e25b90c78de80796b5d063accae5d7a0b484dc8aeb77a0ab9d86b10e9e4af7988c46945'
